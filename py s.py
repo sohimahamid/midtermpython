09fe1f0f65c93f92afcd538f4a7eef28af4d6da6
sohimahamid <156577491+sohimahamid@users.noreply.github.com>
2024-01-14 15:15:42 +0500

    Add files via upload

---
+++ py s.py
@@ -0,0 +1,25 @@
+def hey ():
+ q = input("enter addition subtraction multiplication divide ")
+ t = input ("enter your number 1")
+ h = input ("enter your number 2")
+ l=addition
+ m=subtraction
+ k=multiplication
+ n=divide
+ if  q==l:
+  print ("t+h")
+ elif q==m:
+  print ("t-h")
+ elif q==k:
+  print("t*h")
+ elif q==n:
+  print ("t/h")
+hey()
+  
+
+
+
+
+
+    
+             
